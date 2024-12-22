05fc0feb8bd92f86a4bf935e8f4b52138d4ee0ac
Mahmoud8513 <114151982+Mahmoud8513@users.noreply.github.com>
2024-12-22 19:57:46 +0200

    Adding Game Logic And Screen Files

---
+++ Game/gamelogic.d
@@ -0,0 +1,4 @@
+.\gamelogic.o: GameLogic.c
+.\gamelogic.o: GameLogic.h
+.\gamelogic.o: ../Game/tm4c123gh6pm.h
+.\gamelogic.o: ../Game/Nokia5110.h
---
+++ Game/uart_interrupt.d
@@ -0,0 +1,6 @@
+.\uart_interrupt.o: ..\Drivers\UART_INTERRUPT.c
+.\uart_interrupt.o: ..\Game/tm4c123gh6pm.h
+.\uart_interrupt.o: ..\Drivers\UART_INTERRUPT.h
+.\uart_interrupt.o: ..\Drivers\../Game/Nokia5110.h
+.\uart_interrupt.o: ..\Drivers/PortFConfg.h
+.\uart_interrupt.o: ..\Drivers/Portf_Driver.h
